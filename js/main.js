677dc227dfa5b7288e4c33f99e3446d377a757fc
Quinn Rohlf <qr@qrohlf.com>
2014-04-17 22:22:26 -0700

    ga update

--- js/main.js
+++ js/main.js
@@ -4,4 +4,5 @@
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-40241848-1', 'qrohlf.com');
+  ga('require', 'displayfeatures');
   ga('send', 'pageview');
